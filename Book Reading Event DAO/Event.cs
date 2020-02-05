fcc7ef262be6f8b41dfb5918c29ee81545b66cc4
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-05 14:35:40 +0530

    Event Model

---
+++ Book Reading Event DAO/Event.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Reading_Event_DAO
+{
+    class Event
+    {
+        [Key]
+        public int EventId { get; set; }
+
+        [Required]
+        public string EventName { get; set; }
+
+        public string EventDescription { get; set; }
+
+        public string EventOtherDetails { get; set; }
+
+        [Required]
+        public string EventLocation { get; set; }
+
+        [Required]
+        public string EventDuration { get; set; }
+
+        [Required]
+        public int EventType { get; set; }
+
+        public string EventDate { get; set; }
+
+        public string EventStartTime { get; set; }
+
+    }
+}
