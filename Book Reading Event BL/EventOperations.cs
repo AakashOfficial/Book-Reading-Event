44fc3370ad74cde23d1f66af70f214fc4522f603
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-06 20:54:16 +0530

    Event Business Logic

---
+++ Book Reading Event BL/EventOperations.cs
@@ -0,0 +1,26 @@
+﻿using Book_Reading_Event_DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Reading_Event_BL
+{
+    public class EventOperations
+    {
+        bool result = false;
+        private BookReadingContext db;
+        public EventOperations()
+        {
+            db = new BookReadingContext();
+        }
+
+        public Boolean addEvents(Event ev) {
+
+            db.events.Add(ev);
+            return result;
+        }
+
+    }
+}
