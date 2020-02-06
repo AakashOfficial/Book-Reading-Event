1838779aaef230782f471b01c9b1037da5a56445
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-06 09:56:21 +0530

    User Model

---
+++ Book Reading Event DAO/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Reading_Event_DAO
+{
+    class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string UserEmail { get; set; }
+
+        [Required]
+        public string UserPassword { get; set; }
+    }
+}
