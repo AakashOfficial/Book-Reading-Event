7a5b47815fb34d13327747f072350985e2aab32a
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-06 12:46:43 +0530

    Database Context File

---
+++ Book Reading Event DAO/BookReadingContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Reading_Event_DAO
+{
+    public class BookReadingContext : DbContext
+    {
+        public DbSet<Event> events { get; set; }
+
+        public DbSet<Comment> comment { get; set; }
+
+        public DbSet<Invitation> invitation { get; set; }
+
+        public DbSet<User> user { get; set; }
+
+    }
+}
