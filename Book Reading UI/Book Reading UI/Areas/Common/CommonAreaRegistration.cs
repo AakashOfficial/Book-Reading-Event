6f7bd960d15a6a18b651ac8e661ce4e751c68b95
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-06 11:29:57 +0530

    Common Area

---
+++ Book Reading UI/Book Reading UI/Areas/Common/CommonAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Book_Reading_UI.Areas.Common
+{
+    public class CommonAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Common";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Common_default",
+                "Common/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
