1c4ba31ff74affa0c0f506035e49732a20fbd612
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-06 09:59:35 +0530

    Comment Model

---
+++ Book Reading Event DAO/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Reading_Event_DAO
+{
+    class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserComment { get; set; }
+    }
+}
