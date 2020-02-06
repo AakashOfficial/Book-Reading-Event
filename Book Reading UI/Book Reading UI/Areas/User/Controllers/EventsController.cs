54e8fa79aeb56031d376eeeba76bed3b8de47288
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-06 12:47:30 +0530

    Events Controller

---
+++ Book Reading UI/Book Reading UI/Areas/User/Controllers/EventsController.cs
@@ -0,0 +1,24 @@
+﻿using Book_Reading_Event_DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Book_Reading_UI.Areas.User.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: User/Events
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Event events){
+
+            return View();
+        }
+    }
+}
