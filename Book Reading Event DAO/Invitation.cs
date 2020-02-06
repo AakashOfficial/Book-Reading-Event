5ad13678030e9b16b0a0dc70f7ea31aaeac7fe59
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-06 09:51:54 +0530

    Invitation Model

---
+++ Book Reading Event DAO/Invitation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Reading_Event_DAO
+{
+    class Invitation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int InvitationActive { get; set; }
+
+    }
+}
