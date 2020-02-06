2392ef6859ab2b2039ec00ec9c1f60bfdbe0aa27
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-06 20:52:57 +0530

    User Model

--- Book Reading Event DAO/User.cs
+++ Book Reading Event DAO/User.cs
@@ -23,7 +23,7 @@ namespace Book_Reading_Event_DAO
 
         public string UserRole { get; set; }
 
-        public virtual IList<Comment> Comments { get; set; }
+        // public virtual IList<Comment> Comments { get; set; }
 
         public virtual IList<Event> Events { get; set; }
     }
