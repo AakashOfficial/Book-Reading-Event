abc972bc53fd17bfea38c94d76a68251222282e8
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-09 21:20:32 +0530

    User Add DAO

---
+++ Book Reading Event DAO/UserOperation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Reading_Event_DAO {
+
+    class UserOperation {
+
+        private BookReadingContext db;
+
+        public UserOperation() {
+            db = new BookReadingContext();
+        }
+
+        public bool addUser(User us) {
+            if (us == null) {
+                return false;
+            }
+            db.user.Add(us);
+            db.SaveChanges();
+            return true;
+        }
+    }
+}
