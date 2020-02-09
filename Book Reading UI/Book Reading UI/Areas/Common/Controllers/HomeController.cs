b12ac0e8adb67fc070ef3243fe9ef415c686e70f
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-09 20:15:34 +0530

    Home Controller

---
+++ Book Reading UI/Book Reading UI/Areas/Common/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Book_Reading_UI.Areas.Common.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Common/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
