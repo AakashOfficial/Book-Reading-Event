81a6e3ed6865748f6bd6901894d7bebc32c1f5fa
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-09 20:22:43 +0530

    Authentication Controller

---
+++ Book Reading UI/Book Reading UI/Areas/Security/Controllers/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Book_Reading_UI.Areas.Security.Controllers
+{
+    [AllowAnonymous]
+    public class AuthenticationController : Controller
+    {
+        // GET: Security/Authentication
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register() {
+
+            return View();
+        }
+    }
+}
