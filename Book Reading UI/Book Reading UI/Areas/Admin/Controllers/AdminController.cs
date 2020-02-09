2aa9ec77f15149bb39fdee254ae440f0b9a96e64
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-09 20:14:47 +0530

    Admin Controller

---
+++ Book Reading UI/Book Reading UI/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Book_Reading_UI.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin/Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
