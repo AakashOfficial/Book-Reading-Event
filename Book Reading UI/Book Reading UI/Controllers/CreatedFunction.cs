945fd6fd40fa8e23c68fb72231a92c64a71df0f6
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-10 16:58:27 +0530

    Internal Function For Check user is Logged in

---
+++ Book Reading UI/Book Reading UI/Controllers/CreatedFunction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Book_Reading_UI.Controllers {
+
+    public class CreatedFunction :Controller {
+
+        public bool CheckLoginUser() {
+            int userId = (int)System.Web.HttpContext.Current.Session["userId"];
+            
+            if (userId > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        
+    }
+}
