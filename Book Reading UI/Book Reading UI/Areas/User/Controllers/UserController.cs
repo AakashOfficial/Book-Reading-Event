657a3d43d727e9b69c8b96073013ef2d865b56bb
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-10 11:01:18 +0530

    User Controller

---
+++ Book Reading UI/Book Reading UI/Areas/User/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Book_Reading_UI.Areas.User.Controllers {
+
+    public class UserController : Controller {
+
+        // GET: User/User
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
