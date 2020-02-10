5af10718e3e876d57c0e794391332ec5db1a31d3
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-10 10:45:31 +0530

    User DAO Object Added

--- Book Reading UI/Book Reading UI/Areas/Security/Controllers/AuthenticationController.cs
+++ Book Reading UI/Book Reading UI/Areas/Security/Controllers/AuthenticationController.cs
@@ -1,23 +1,22 @@
-﻿using System;
+﻿using Book_Reading_Event_BL;
+using Book_Reading_Event_DAO;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Book_Reading_UI.Areas.Security.Controllers
-{
-    [AllowAnonymous]
-    public class AuthenticationController : Controller
-    {
-        // GET: Security/Authentication
-        public ActionResult Login()
-        {
-            return View();
+namespace Book_Reading_UI.Areas.Security.Controllers {
+
+    // [AllowAnonymous]
+    public class AuthenticationController : Controller {
+
+        private UserOperation uo;
+
+        public AuthenticationController(){
+            uo = new UserOperation();
         }
 
-        public ActionResult Register() {
-
-            return View();
-        }
-    }
+            }
 }
