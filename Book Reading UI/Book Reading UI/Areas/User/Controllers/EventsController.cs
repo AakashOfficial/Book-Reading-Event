32096a4f6f66003088f06abcc0464b030f01c371
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-10 17:05:06 +0530

    Event Controller

--- Book Reading UI/Book Reading UI/Areas/User/Controllers/EventsController.cs
+++ Book Reading UI/Book Reading UI/Areas/User/Controllers/EventsController.cs
@@ -1,5 +1,6 @@
 ﻿using Book_Reading_Event_BL;
 using Book_Reading_Event_DAO;
+using Book_Reading_UI.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,32 +14,55 @@ namespace Book_Reading_UI.Areas.User.Controllers {
         private bool result = false;
 
         private EventOperations evop;
+        private CreatedFunction cf;
 
         public EventsController(){
             evop = new EventOperations();
+            cf = new CreatedFunction();
         }
 
         // GET: User/Events
-        public ActionResult Index() {
+        public ActionResult Index()
+        {
+            
+            var test = cf.CheckLoginUser();
+            if (!test) {
+                return Redirect("/Security/Authentication/Login");
+            }
+
             return View();
         }
 
         [HttpPost]
         public ActionResult Create(Event events){
+            var test = cf.CheckLoginUser();
+            if (!test) {
+                return Redirect("/Security/Authentication/Login");
+            }
+
+            events.UserId = cf.LoggedUserId();
 
             result = evop.addEvents(events);
 
             return RedirectToAction("Index");
         }
 
+        // Function To View Edit Form of Event
         public ActionResult Edit(int eventid) {
 
-            if (eventid == null)
-            {
+            if (eventid == null) {
                 return HttpNotFound();
             }
 
             return View();
         }
+
+        // Function To Edit the Event
+        public ActionResult UpdateEvent(Event ev) {
+
+            return View();
+        }
+
+        
     }
 }
