bbaf2baf2f8e27433e339832641d1bda020394fe
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-10 10:40:44 +0530

    Backend Object

---
+++ Book Reading Event BL/UserOperation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Reading_Event_DAO {
+
+    public class UserOperation {
+
+        private BookReadingContext db;
+
+        public UserOperation() {
+            db = new BookReadingContext();
+        }
+
+        
+    }
+}
