33c179a3367353161407981a14aa3e8a59e20626
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-12 16:20:07 +0530

    Home Controller

--- Book Reading UI/Book Reading UI/Areas/Common/Controllers/HomeController.cs
+++ Book Reading UI/Book Reading UI/Areas/Common/Controllers/HomeController.cs
@@ -1,18 +1,26 @@
-﻿using System;
+﻿using Book_Reading_Event_BL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Book_Reading_UI.Areas.Common.Controllers
-{
-    public class HomeController : Controller
-    {
+namespace Book_Reading_UI.Areas.Common.Controllers {
+
+    public class HomeController : Controller {
+
+        private EventOperations eventOperations;
+
+        public HomeController() {
+            eventOperations = new EventOperations();
+        }
+
         // GET: Common/Home
         public ActionResult Index() {
             Session["userId"] = 0;
-            // System.Diagnostics.Debug.WriteLine((int)Session["userId"]);
-            return View();
+            var output = eventOperations.getPublicEvent();
+
+            return View(output);
         }
     }
 }
