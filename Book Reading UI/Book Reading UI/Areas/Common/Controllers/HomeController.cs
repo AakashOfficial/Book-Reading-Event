85f9590f3ff27c8d910d880f3976dbe62711dd73
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-12 11:29:54 +0530

    Home Controller

--- Book Reading UI/Book Reading UI/Areas/Common/Controllers/HomeController.cs
+++ Book Reading UI/Book Reading UI/Areas/Common/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace Book_Reading_UI.Areas.Common.Controllers
         // GET: Common/Home
         public ActionResult Index() {
             Session["userId"] = 0;
-            System.Diagnostics.Debug.WriteLine((int)Session["userId"]);
+            // System.Diagnostics.Debug.WriteLine((int)Session["userId"]);
             return View();
         }
     }
