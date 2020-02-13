3daf8becd98691ba88e9afc160af15ed842e35ea
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-13 12:10:07 +0530

    Home Test @HomeController

---
+++ Book Reading Test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Reading_Test.Controllers {
+
+    [TestClass]
+    class HomeControllerTest {
+
+        [TestMethod]
+        public void Index() {
+
+        }
+
+    }
+}
