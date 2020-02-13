25987bb6d627e63caf95ecc1ddaa5ec40cf8c67a
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-13 11:58:17 +0530

    Index Test @EventsController

--- Book Reading Test/Controllers/EventsControllerTest.cs
+++ Book Reading Test/Controllers/EventsControllerTest.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Book_Reading_UI.Areas.User.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Book_Reading_Test.Controllers
-{
-    class EventsControllerTest
-    {
+namespace Book_Reading_Test.Controllers {
+
+    [TestClass]
+    class EventsControllerTest {
+
+        [TestMethod]
+        public void Index() {
+
+            // Arrange
+            EventsController controller = new EventsController();
+            string viewName = "Index";
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual(viewName, result.ViewName);
+        }
     }
 }
