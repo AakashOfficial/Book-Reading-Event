3132a5b06aa18e72974aac44453182df766b2ec9
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-13 11:49:31 +0530

    Admin Controller Unit Tests

---
+++ Book Reading Test/Controllers/AdminControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using Book_Reading_UI.Areas.Admin.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// using System.Web.Mvc;
+
+namespace Book_Reading_Test.Controllers {
+
+    [TestClass]
+    public class AdminControllerTest {
+
+        [TestMethod]
+        public void AllUsers()
+        {
+            // Arrange
+            AdminController controller = new AdminController();
+            string viewName = "AllUsers";
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual(viewName, result.ViewName);
+        }
+
+
+    }
+}
