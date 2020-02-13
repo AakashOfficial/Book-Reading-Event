f98fdbba70a4387adce407880e5901675d4982c9
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-13 12:24:31 +0530

    Invitations Test @EventsController

--- Book Reading Test/Controllers/EventsControllerTest.cs
+++ Book Reading Test/Controllers/EventsControllerTest.cs
@@ -85,7 +85,7 @@ namespace Book_Reading_Test.Controllers {
             string viewName = "Invitations";
 
             // Act
-            ViewResult result = controller.Index() as ViewResult;
+            ViewResult result = controller.Invitations() as ViewResult;
 
             // Assert
             Assert.AreEqual(viewName, result.ViewName);
