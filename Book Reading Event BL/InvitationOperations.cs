eb4c69e46c68a79d69fc1067a6dd2ae4bfafed00
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-13 10:16:42 +0530

    Invitation DAO

--- Book Reading Event BL/InvitationOperations.cs
+++ Book Reading Event BL/InvitationOperations.cs
@@ -25,5 +25,22 @@ namespace Book_Reading_Event_BL {
             var output = db.invitation.ToList().Where(d=>d.UserId == userId);
             return output;
         }
+
+        // Function To Invite the User
+        public bool inviteUser(int eventId, int[] userId) {
+
+            Invitation invitation = new Invitation();
+
+            for (int i = 0; i < userId.Length; i++) {
+                invitation.EventId = eventId;
+                invitation.UserId = userId[i];
+                invitation.InvitationActive = 1;
+
+                db.invitation.Add(invitation);
+                db.SaveChanges();
+            }
+            return true;
+        }
+
     }
 }
