c772dda36b14a9f7168681f252563471f0bd0062
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-14 12:35:19 +0530

    Project Complete

---
+++ Book Reading Event DAO/Migrations/202002140543305_test.cs
@@ -0,0 +1,20 @@
+﻿namespace Book_Reading_Event_DAO.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class test : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Events", "EventDescription", c => c.String(nullable: false));
+            AlterColumn("dbo.Events", "EventStartTime", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Events", "EventStartTime", c => c.String());
+            AlterColumn("dbo.Events", "EventDescription", c => c.String());
+        }
+    }
+}
